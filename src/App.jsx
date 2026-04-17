896c89ee5a222fadca978c665461ea0674eb81eb
chandanr54 <67356198+chandanr54@users.noreply.github.com>
2026-04-17 17:24:23 +0530

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import Home from './compenent/Home'
-import { HashRouter, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
