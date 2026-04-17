03aca10a4caa9f99d082f47f3c489d45610b3316
chandanr54 <67356198+chandanr54@users.noreply.github.com>
2026-04-17 17:26:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-import Home from './compenent/Home'
+import Home from './pages/Home'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
